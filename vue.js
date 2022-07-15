e328e630956ff0336bab28e24abfe4190689f4a7
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-07-15 15:55:46 +0300

    неудачная попытка вынести форму в компонент

--- vue.js
+++ vue.js
@@ -1,10 +1,28 @@
 
+Vue.component('add-new-task', {
+    
+    props: ['value'],
+    template: `
+    <form class="add-task">
+        <label for="task-name"> Название задачи</label>
+        <input type="text" name="task-name" 
+            v-bind:value = "value"
+            v-on:input="$emit('input', $event.target.value)"  
+        >
+        <button class="task__add" v-on:click.prevent="$emit('addNewTask')">Добавить задачу</button>  
+    </form>
+    `,
+   
+})
+
+
+
 Vue.component ('new-task' , {
     props: ['task'],
     template: `
         <div class = 'task__item'>
             <input type = 'checkbox' name = 'task-status'>
-            <p class = 'task-name'>{{task}}</p>
+            <p class = 'task-name'>{{task.name}}</p>
         </div>
     `,
     
@@ -17,12 +35,10 @@ const vm = new Vue({
         taskNameList: [],   
     },
 
-    
     methods: {
         addNewTask: function () {
             this.taskNameList.push({name: this.taskName})
-            return this.taskNameList
-       } 
+        } 
     }
     
 })
