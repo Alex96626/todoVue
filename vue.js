d653084976c0305cfe7a090a839d5e370c4045f9
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-07-18 13:01:41 +0300

    пробы $emit

--- vue.js
+++ vue.js
@@ -2,56 +2,27 @@
 Vue.component('add-new-task', {
     
     props: ['value'],
-    
-    data: function() {
-        return {
-            taskName: '',
-            taskNameList: [], 
-        }
-    },
-    methods: {
-        addNewTask: function () {
-            console.log(vm.taskName)
-            vm.taskNameList.push({name: vm.taskName})
-        },
-
-        
-    },
 
     template: `
     <form class="add-task">
         <label for="task-name"> Название задачи</label>
         <input type="text" name="task-name" 
             v-bind:value = "value"
-            v-on:input="$emit('input',$event.target.value)"
-            
+            v-on:input="$emit('input',$event.target.value)" 
         >
-        <button class="task__add" v-on:click.prevent='addNewTask'>Добавить задачу</button>  
+        <button class="task__add" v-on:click.prevent="$emit('addtask')">Добавить задачу</button>  
     </form>
     `,
 })
 
 Vue.component ('new-task' , {
-    props: ['task', 'checked'],
+    props: ['task'],
     template: `
-        <div class = 'task__item' v-bind:class="{active:isActive}">
-            <input type = 'checkbox' name = 'task-status' v-on:click='finishTask'>
+        <div class = 'task__item'>
+            <input type = 'checkbox' name = 'task-status' v-on:click="$emit('closetask', $event)">
             <p class = 'task-name'>{{task.name}}</p>
         </div>
     `,
-
-    data: function() {
-        return {
-            isActive: false,
-        }
-    },
-    
-    methods: {
-        finishTask: function(event) {
-            event.target.checked ? this.isActive = true: this.isActive = false
-
-        }
-    }
     
 })
 
@@ -61,6 +32,20 @@ const vm = new Vue({
         taskName: '' ,
         taskNameList: [], 
         isActive: false,
+    },
+
+    methods: {
+        addNewTask: function () {
+            this.taskNameList.push({name: this.taskName})
+        },
+
+        finishTask: function(event) {
+            console.log(event)
+            event.target.checked ? this.isActive = true : this.isActive = false
+
+        }
+
+        
     },
 
 })
